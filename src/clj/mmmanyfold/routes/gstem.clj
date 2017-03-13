c8154f527540a7f55412b84645be6bb0f5274652
dviramontes <dviramontes@gmail.com>
2017-03-13 00:08:20 -0600

    chore: clean-up requires

--- src/clj/mmmanyfold/routes/gstem.clj
+++ src/clj/mmmanyfold/routes/gstem.clj
@@ -1,22 +1,15 @@
 (ns mmmanyfold.routes.gstem
-  (:require
-    [compojure.api.sweet :refer [context GET POST defroutes]]
-    [environ.core :refer [env]]
-    [clj-facebook-graph.auth :refer [with-facebook-auth]]
-    [clj-facebook-graph [client :as client]]
-    [ring.util.response :refer [response]]
-    [org.httpkit.client :as http]
-    [ring.util.http-response :refer [ok]]
-    [clojure.core.async :refer [>! <! >!! <!! go chan]]
-    [cheshire.core :as json]))
+  (:require [compojure.api.sweet :refer [context GET defroutes]]
+            [clj-facebook-graph.auth :refer [with-facebook-auth]]
+            [clj-facebook-graph [client :as client]]
+            [ring.util.http-response :refer [ok]]))
 
 (def ac {:access-token (System/getenv "GSTEM_FB_PAGE_ACCESS_TOKEN")})
 
-(defn get-fb-feed [request]
+(defn get-fb-feed [_]
   (let [feed-request (with-facebook-auth ac (client/get [:me :feed] {:query-params {:limit 10} :extract :data}))]
     (let [filtered (filterv :message feed-request)
-          feed-resources
-          (filterv #(re-find #"(?i)\#resources?" (:message %)) filtered)]
+          feed-resources (filterv #(re-find #"(?i)#resources?" (:message %)) filtered)]
       (ok feed-resources))))
 
 (defroutes gstem-routes
