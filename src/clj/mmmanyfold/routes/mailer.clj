dc8db963346649e6ad2ae56b6caac282485f6192
dviramontes <dviramontes@gmail.com>
2017-04-14 18:55:18 -0600

    wip/feat: email attachment

--- src/clj/mmmanyfold/routes/mailer.clj
+++ src/clj/mmmanyfold/routes/mailer.clj
@@ -1,7 +1,9 @@
 (ns mmmanyfold.routes.mailer
   (:require [mailgun.mail :as mail]
+            [mailgun.util :refer [to-file]]
             [compojure.core :refer [defroutes POST]]
-            [ring.util.http-response :refer :all]))
+            [ring.util.http-response :refer :all]
+            [clojure.java.io :as io]))
 
 ;; TODO: get :domain from params map as well
 (def creds {:key    (System/getenv "PLAYGROUND_MAILGUN_API_KEY")
@@ -9,15 +11,27 @@
 
 (defn handle-posting-via-mailgun [req]
   (let [{:keys [body subject to from]} (:params req)
-        mail-transact!
-        (mail/send-mail creds
-                        {:from    from
-                         :to      to
-                         :subject subject
-                         :html    body})]
-    (if (= (:status mail-transact!) 200)
-      (ok mail-transact!)
-      (internal-server-error mail-transact!))))
+        opts {:from    from
+              :to      to
+              :subject subject
+              :html    body}]
+    (if-let [attachment (get-in req [:params :attachment])]
+      (let [temp-csv (java.io.File/createTempFile "temp" ".csv")
+            file-path (.getAbsolutePath temp-csv)
+            _ (spit temp-csv attachment)
+            opts {:attachment (to-file [file-path])
+                  :from       from
+                  :to         to
+                  :subject    subject
+                  :html       body}
+            mail-transact! (mail/send-mail creds opts)]
+        (if (= (:status mail-transact!) 200)
+          (ok mail-transact!)
+          (internal-server-error mail-transact!)))
+      (let [mail-transact! (mail/send-mail creds opts)]
+        (if (= (:status mail-transact!) 200)
+          (ok mail-transact!)
+          (internal-server-error mail-transact!))))))
 
 (defroutes mailer-routes
            (POST "/mail" {params :params} handle-posting-via-mailgun))
