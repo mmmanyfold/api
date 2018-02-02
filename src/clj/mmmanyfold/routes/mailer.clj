72720788fc00f4bdb60f84e0dad54c114972960d
eemshi <oreneemshi@gmail.com>
2018-02-01 18:11:27 -0700

    adds file attachment to mailer

--- src/clj/mmmanyfold/routes/mailer.clj
+++ src/clj/mmmanyfold/routes/mailer.clj
@@ -1,11 +1,13 @@
 (ns mmmanyfold.routes.mailer
   (:require [mailgun.mail :as mail]
+            [mailgun.util :refer to-file]
+            [clojure.java.io :as io]
             [compojure.core :refer [defroutes POST]]
             [ring.util.http-response :refer :all]))
 
 ;; TODO: get :domain from params map as well
-(def creds {:key    (System/getenv "PLAYGROUND_MAILGUN_API_KEY")
-            :domain "playgroundcoffeeshop.com"})
+(def creds {:key    (System/getenv "MMM_MAILGUN_API_KEY")
+            :domain "mg.codefordenver.org"})
 
 (defn handle-posting-via-mailgun [req]
   (let [{:keys [body subject to from]} (:params req)
@@ -19,5 +21,22 @@
       (ok mail-transact!)
       (internal-server-error mail-transact!))))
 
+;; TODO: refactor this into handle-posting-via-mailgun (single endpoint)
+(defn handle-posting-file-via-mailgun [req]
+  (let [{:keys [body subject file to from]} (:params req)
+        _ (println (.getAbsolutePath (:tempfile file)))
+        mail-transact!
+        (mail/send-mail {:key (System/getenv "MMM_MAILGUN_API_KEY")
+                         :domain "mg.codefordenver.org"}
+                        {:from       from
+                         :to         to
+                         :attachment (to-file [(.getAbsolutePath (:tempfile file))])
+                         :subject    subject
+                         :html       body})]
+    (if (= (:status mail-transact!) 200)
+      (ok mail-transact!)
+      (internal-server-error mail-transact!))))
+
 (defroutes mailer-routes
-           (POST "/mail" {params :params} handle-posting-via-mailgun))
+           (POST "/mail" {params :params} handle-posting-via-mailgun)
+           (POST "/mail-file" {params :params} handle-posting-file-via-mailgun))
